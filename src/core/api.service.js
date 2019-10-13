5afcbe075a41d907383ef521e6f4d4873b7c5554
noyaf1er <yannickossuih@gmail.com>
2019-10-13 13:28:50 +0200

    feat(api): set up api service

--- src/core/api.service.js
+++ src/core/api.service.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import { API_URL, API_SUFFIX } from "./config";
+import API_URL, { API_CREDENTIALS } from "./config";
 
 const ApiService = {
   init() {
@@ -9,45 +9,28 @@ const ApiService = {
     Vue.axios.defaults.baseURL = API_URL;
   },
 
-  query(resource, params) {
-    return Vue.axios.get(resource, params).catch(error => {
+  get(resource, slug = null, queryParams = {}) {
+    const url = slug ? `${resource}/${slug}` : `${resource}`;
+    const params = { ...API_CREDENTIALS, ...queryParams };
+
+    return Vue.axios.get(url, { params }).catch(error => {
       // console.warn(error);
       throw new Error(`[MARVEL] ApiService ${error}`);
     });
   },
-
-  get(resource, slug = null) {
-    let url = slug ? `${resource}/${slug}` : `${resource}`;
-    url += API_SUFFIX;
-    return Vue.axios.get(url).catch(error => {
-      // console.warn(error);
-      throw new Error(`[MARVEL] ApiService ${error}`);
-    });
-  },
-
-  post(resource, params) {
-    return Vue.axios.post(`${resource}`, params);
-  },
-
-  update(resource, slug, params) {
-    return Vue.axios.put(`${resource}/${slug}`, params);
-  },
-
-  put(resource, params) {
-    return Vue.axios.put(`${resource}`, params);
-  },
-
-  delete(resource) {
-    return Vue.axios.delete(resource).catch(error => {
-      throw new Error(`[MARVEL] ApiService ${error}`);
-    });
-  }
 };
 
 export default ApiService;
 
 export const CharactersService = {
-  get(slug = null) {
-    return ApiService.get("characters", slug);
+  async get(slug = null, offset = 0, limit = 20) {
+    const params = { offset, limit };
+
+    return await ApiService.get('characters', slug, params);
+  },
+
+  getThumbnail(ressource) {
+
   }
+
 };
--- src/core/config.js
+++ src/core/config.js
@@ -1,10 +1,11 @@
-const TS = "1"
-// eslint-disable-next-line
-const PRIVATE_KEY = 'c83b409edc5238142b6c35c90e950765a82cd9a7';
-const PUBLIC_KEY = 'ba8459e826c5e95745dc72a09cd3d370';
-const HASH = "0ac93170a108ef00ce3c5baada8c6d3f" // md5(TS+PRIVATE_KEY+PUBLIC_KEY)
+// const PRIVATE_KEY = 'c83b409edc5238142b6c35c90e950765a82cd9a7';
+// const PUBLIC_KEY = 'ba8459e826c5e95745dc72a09cd3d370';
 
-export const API_SUFFIX = `?ts=${TS}&apikey=${PUBLIC_KEY}&hash=${HASH}`;
+export const API_CREDENTIALS = {
+    ts: '1',
+    apikey: 'ba8459e826c5e95745dc72a09cd3d370',
+    hash: '0ac93170a108ef00ce3c5baada8c6d3f'
+};
 export const API_URL = "https://gateway.marvel.com/v1/public/";
 
 export default API_URL;
